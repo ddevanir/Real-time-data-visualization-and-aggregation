72b991bdc3c93f4b314ad396dc3f7ab42e53e71c
ktitan123 <kevinsebastian@dhcp-v060-212.mobile.uci.edu>
2017-04-25 19:35:44 -0700

    Branch

--- src/main/java/app/mapReduce/mapReduce.java
+++ src/main/java/app/mapReduce/mapReduce.java
@@ -2,12 +2,22 @@ package app.mapReduce;
 
 /**
  * Created by sbr on 4/25/17.
+ * https://spark.apache.org/docs/2.1.0/quick-start.html
  */
 import static spark.Spark.*;
-
+import java.util.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.TaskContext;
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka010.*;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class mapReduce {
-
+    
 
 
 }
