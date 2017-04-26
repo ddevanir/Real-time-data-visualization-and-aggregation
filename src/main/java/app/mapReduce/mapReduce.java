cd9dd80e73dd5251ad6be388ae443ee4772647ec
ktitan123 <kevinsebastian@dhcp-v060-212.mobile.uci.edu>
2017-04-25 19:44:24 -0700

    Spark

--- src/main/java/app/mapReduce/mapReduce.java
+++ src/main/java/app/mapReduce/mapReduce.java
@@ -2,12 +2,33 @@ package app.mapReduce;
 
 /**
  * Created by sbr on 4/25/17.
+ * https://spark.apache.org/docs/2.1.0/quick-start.html
  */
 import static spark.Spark.*;
-
+import java.util.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.TaskContext;
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka010.*;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class mapReduce {
 
+    public void initializekafka()
+    {
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("bootstrap.servers", "localhost:9092");
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put("group.id", "use_a_separate_group_id_for_each_stream");
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
+        Collection<String> topics = Arrays.asList("air", "weather","traffic");
+    }
 
 
 }
