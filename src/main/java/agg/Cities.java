e160af70654d0417a283dda56088f3f5b6303a5c
Devanira <ddevanir@uci.edu>
2017-06-10 19:35:32 -0700

    cities

--- src/main/java/agg/Cities.java
+++ src/main/java/agg/Cities.java
@@ -78,8 +78,59 @@ public class Cities {
         getLatLon.put("Atlanta","33.7490,84.3880");
         citiesbybb.put("Atlanta","33.647808%2C-84.551819%2C33.887618%2C-84.289389");
 
+        cities.add("Vancouver");
+        getID.put("Vancouver",14);
+        getLatLon.put("Vancouver","49.2827,-123.1207");
+        citiesbybb.put("Vancouver","49.198177%2C-123.224740%2C49.317294%2C-123.023068");
 
+        cities.add("Las%20Vegas");
+        getID.put("Las%20Vegas",15);
+        getLatLon.put("Las%20Vegas","36.1699,-115.1398");
+        citiesbybb.put("Las%20Vegas","36.129623%2C-115.414625%2C36.380623%2C-115.062072");
 
+        cities.add("Dallas");
+        getID.put("Dallas",16);
+        getLatLon.put("Dallas","32.7767,-96.7970");
+        citiesbybb.put("Dallas","32.617537%2C-96.999347%2C33.023792%2C-96.463738");
 
+        cities.add("Ottawa");
+        getID.put("Ottawa",17);
+        getLatLon.put("Ottawa","45.4215,-75.6972");
+        citiesbybb.put("Ottawa","44.962733%2C-76.353916%2C45.537580%2C-75.246598");
+
+        cities.add("Miami");
+        getID.put("Miami",18);
+        getLatLon.put("Miami","25.7617,-80.1918");
+        citiesbybb.put("Miami","25.709042%2C-80.319760%2C25.855773%2C-80.139157");
+
+        cities.add("Mexico%20City");
+        getID.put("Mexico%20City",19);
+        getLatLon.put("Mexico%20City","19.4326,-99.1332");
+        citiesbybb.put("Mexico%20City","19.188710%2C-99.326777%2C19.592757%2C-98.960448");
+
+        cities.add("Lima");
+        getID.put("Lima",20);
+        getLatLon.put("Lima","-12.0464,77.0428");
+        citiesbybb.put("Lima","-12.253289%2C-77.187219%2C-11.799988%2C-76.788340");
+
+        cities.add("Buenos%20Aires");
+        getID.put("Buenos%20Aires",21);
+        getLatLon.put("Buenos%20Aires","-34.6037,58.3816");
+        citiesbybb.put("Buenos%20Aires","-34.705101%2C-58.531452%2C-34.526546%2C-58.335145");
+
+        cities.add("Calgary");
+        getID.put("Calgary",22);
+        getLatLon.put("Calgary","51.0486,-114.0708");
+        citiesbybb.put("Calgary","50.842404%2C-114.271360%2C51.183830%2C-113.876951");
+
+        cities.add("Winnipeg");
+        getID.put("Winnipeg",23);
+        getLatLon.put("Winnipeg","49.8951,-97.1384");
+        citiesbybb.put("Winnipeg","49.713770%2C-97.349222%2C49.993874%2C-96.955228");
+
+        cities.add("Winnipeg");
+        getID.put("Winnipeg",24);
+        getLatLon.put("Winnipeg","49.8951,-97.1384");
+        citiesbybb.put("Winnipeg","49.713770%2C-97.349222%2C49.993874%2C-96.955228");
     }
 }
